f49471aa365021f5e0bd2e566fa02ca084ec2780
Felix-Galle <felixjohngalle@gmail.com>
2025-05-13 17:13:48 +0200

    Started implementing standard_lib.py args, none working yet though.

--- standard_lib.py
+++ standard_lib.py
@@ -1,3 +1,6 @@
+import sys
+import importlib
+
 """
 This will act as the main file for the project.
 It will import the necessary classes and functions from the other files and run the main logic.
@@ -6,3 +9,35 @@ It will also handle the execution of commands and reading files.
 
 
 """
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python standard_lib.py <class_name>.<function_name>")
+        print("Help: python standard_lib.py --help")
+        sys.exit(1)
+
+    try:
+        class_name, func_name = sys.argv[1].rsplit('.', 1)
+        match class_name:
+            case "network_tools":
+                pass
+            case "rev_shell":
+                match func_name:
+                    case "recieve_cmd_udp":
+                        pass
+                    case "send_cmd":
+                        pass
+            case "file_tools":
+                pass
+
+
+        cls = getattr(module, class_name)
+        instance = cls()
+        if hasattr(instance, 'run'):
+            instance.run()
+        else:
+            print(f"The class {class_name} does not have a 'run' method.")
+    except (ImportError, AttributeError) as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
